74def6fa1f9809246dfd2892e163dd7e4720dffd
Ritesh Chaudhari <riteshc7299@gmail.com>
2025-11-03 13:18:38 -0800

    LC3217: Delete Nodes From Linked List Present in Array

---
+++ src/leetcode/solution/LC3217/DeleteNodesFromLinkedListPresentInArray.java
@@ -0,0 +1,31 @@
+package leetcode.solution.LC3217;
+import common.utils.ListNode;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DeleteNodesFromLinkedListPresentInArray {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        ListNode tHead = new ListNode(0);
+        Set<Integer> numsSet = new HashSet<>();
+
+        tHead.next = head;
+
+        for (int num : nums) {
+            numsSet.add(num);
+        }
+
+        ListNode prev = tHead;
+
+        while (head != null) {
+            if (numsSet.contains(head.val)) {
+                prev.next = head.next;
+            } else {
+                prev = head;
+            }
+
+            head = head.next;
+        }
+
+        return tHead.next;
+    }
+}
